23dc4a259eb57c723394dbbd64a26967864f3744
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-03-21 23:26:23 -0300

    Criação do projeto inicial

---
+++ src/main/java/br/com/adriano/controlecartao/ControleCartaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.adriano.controlecartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleCartaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleCartaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/adriano/controlecartao/ControleCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adriano.controlecartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleCartaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
